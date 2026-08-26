3beee78bdd1bf574287069a070c76f80320d0e9d
mattchavess <mateus.f.chaves6@aluno.senai.br>
2026-08-26 16:38:53 -0300

    Atualização de icones

--- moneyball_db.sql
+++ moneyball_db.sql
@@ -16,7 +16,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
+CREATE database moneyball_db;
+use moneyball_db;
 --
 -- Banco de dados: `moneyball_db`
 --
